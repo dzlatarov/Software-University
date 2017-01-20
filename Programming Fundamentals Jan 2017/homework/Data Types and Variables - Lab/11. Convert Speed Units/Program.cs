18a382503a4faa328c3b3a6928606cd39a6490e8
Xadera <ragnarokha@gmail.com>
2017-01-20 23:10:18 +0200

    hw

---
+++ Programming Fundamentals Jan 2017/homework/Data Types and Variables - Lab/11. Convert Speed Units/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Convert_Speed_Units
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distanceMeters = float.Parse(Console.ReadLine());
+            float hours= float.Parse(Console.ReadLine());
+            float minutes = float.Parse(Console.ReadLine());
+            float seconds = float.Parse(Console.ReadLine());
+
+            float meteresPerSecond = distanceMeters / (hours * 3600f + minutes * 60f + seconds);
+            float kilometersPerHour = (distanceMeters / 1000f) / (hours + minutes / 60f + seconds / 3600f);
+            float milesPerHour = (distanceMeters / 1609f) / (hours + minutes / 60f + seconds / 3600f);
+       
+
+            Console.WriteLine("{0:0.########}", meteresPerSecond);
+            Console.WriteLine("{0:0.########}", kilometersPerHour);
+            Console.WriteLine("{0:0.########}", milesPerHour);
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/homework/Data Types and Variables - Lab/12. Rectangle Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Rectangle_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double P = (width + height) * 2;
+            double S = width * height;
+            double D = Math.Sqrt(width * width + height * height);
+
+            Console.WriteLine(P);
+            Console.WriteLine(S);
+            Console.WriteLine(D);           
+        }
+    }
+}
