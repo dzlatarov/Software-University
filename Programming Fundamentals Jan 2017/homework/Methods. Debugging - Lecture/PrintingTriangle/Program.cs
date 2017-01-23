a5d5ba917f5825ae257600ad88944d958079a0e1
Xadera <ragnarokha@gmail.com>
2017-01-23 20:49:51 +0200

    homework, will use better styling from now on

---
+++ Programming Fundamentals Jan 2017/homework/Methods. Debugging - Lecture/PrintingTriangle/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace PrintingTriangle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class PrintingTriangle
+    {
+        public static void Main(string[] args)
+        {
+            PrintTriangle();
+        }
+
+        public static void PrintFirstHalfAndMiddle(int size)
+        {
+            for (int x = 1; x <= size; x++)
+            {
+                for (int y = 1; y <= x; y++)
+                {
+                    Console.Write($"{y} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintSecondHalf(int size)
+        {
+            for (int x = size; x >= 1; x--)
+            {
+                for (int y = 1; y < x; y++)
+                {
+                    Console.Write($"{y} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintTriangle()
+        {
+            int size = int.Parse(Console.ReadLine());
+            PrintFirstHalfAndMiddle(size);
+            PrintSecondHalf(size);
+        }
+    }
+}
