3f08c990c15dec9b41d4642add1fcab9ffedfc97
Xadera <ragnarokha@gmail.com>
2017-01-24 00:10:15 +0200

    mhm

---
+++ Programming Fundamentals Jan 2017/homework/Methods. Debugging - Lecture/8. MultiplyEvensbyOdds/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _8.MultiplyEvensbyOdds
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class EvensByOdds
+    {
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
