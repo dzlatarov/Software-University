fbe5559bb5364cc63e402538c611460bb9289fa9
Xadera <ragnarokha@gmail.com>
2017-01-23 16:38:35 +0200

    hw

---
+++ Programming Fundamentals Jan 2017/homework/Methods. Debugging/1. Blank Receipt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1.Blank_Receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Header();
+            Body();
+            Footer();
+        }
+
+        static void Header()
+        {
+            Console.WriteLine(@"CASH RECEIPT
+------------------------------");
+        }
+
+        static void Body()
+        {
+            Console.WriteLine(@"Charged to____________________
+Received by___________________");
+        }
+
+        static void Footer()
+        {
+            char cThingy = (char)169;
+            Console.WriteLine($"{cThingy} SoftUni");
+        }
+    }
+}
+
