58e6d3f7627105b14414257441f6c408891b9a03
Xadera <ragnarokha@gmail.com>
2017-01-23 21:54:57 +0200

    more homework :)

---
+++ Programming Fundamentals Jan 2017/homework/Methods. Debugging - Lecture/DrawFilledSquare/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DrawFilledSquare
+{
+    using System;
+
+    class FilledSquare
+    {
+        public static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            TopAndBottomSquare(size);
+            SquareFill(size);
+            TopAndBottomSquare(size);
+        }
+
+        public static void TopAndBottomSquare(int size)
+        {
+            Console.WriteLine(new string('-', 2 * size));
+        }
+
+        public static void SquareFill(int size)
+        {
+            string lines = string.Empty;
+
+            for (int i = 1; i < size; i++)
+            {
+                lines = lines + "\\/";
+            }
+
+            for (int i = 0; i < size - 2; i++)
+            {
+                Console.WriteLine("{0}{1}{0}", "-", lines);
+            }
+        }
+    }
+}
