c6822b13e1eb4be339c32355fb629ce359080c8e
Xadera <ragnarokha@gmail.com>
2017-01-24 12:42:32 +0200

    hw update

--- Programming Fundamentals Jan 2017/homework/Methods. Debugging - Lecture/8. MultiplyEvensbyOdds/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Methods. Debugging - Lecture/8. MultiplyEvensbyOdds/Program.cs
@@ -6,12 +6,50 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class EvensByOdds
+    public class EvensTimesOdds
     {
-
         public static void Main(string[] args)
         {
+            int number = int.Parse(Console.ReadLine());
+            int result = GetSumOfEvenDigits(number) * GetSumOfOddDigits(number);
 
+            Console.WriteLine(result);
+        }
+
+        public static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+
+            while (Math.Abs(number) > 0)
+            {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum = sum + lastDigit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        public static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+
+            while (Math.Abs(number) > 0)
+            {
+                int lastDigit = number % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum = sum + lastDigit;
+                }
+
+                number /= 10;
+            }
+
+            return sum;
         }
     }
 }
