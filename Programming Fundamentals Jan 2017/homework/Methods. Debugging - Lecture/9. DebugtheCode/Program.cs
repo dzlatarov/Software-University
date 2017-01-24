353b13b2786a288800fae0d495da7ae9d76be77a
Xadera <ragnarokha@gmail.com>
2017-01-24 13:48:46 +0200

    kanker

---
+++ Programming Fundamentals Jan 2017/homework/Methods. Debugging - Lecture/9. DebugtheCode/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _9.DebugtheCode
+{
+    using System;
+    using System.Globalization;
+
+    public class HolidaysBetweenTwoDates
+    {
+        public static void Main()
+        {
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "d.M.yyyy", CultureInfo.InvariantCulture);
+
+            int holidaysCount = 0;
+            for (; startDate <= endDate; startDate = startDate.AddDays(1))
+            {
+                if (startDate.DayOfWeek == DayOfWeek.Saturday || startDate.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                }
+            }
+
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
