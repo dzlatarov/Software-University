6e565a5b191305ef0ec3382d358208aa837bc823
Xadera <ragnarokha@gmail.com>
2017-01-26 01:53:40 +0200

    fix

--- Programming Fundamentals Jan 2017/homework/Methods, Debugging - Lab/Methods, Debugging - Lab/09. LongerLine/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Methods, Debugging - Lab/Methods, Debugging - Lab/09. LongerLine/Program.cs
@@ -1,70 +1,56 @@
-﻿namespace _09.LongerLine
+﻿namespace _09. LongerLine
 {
     using System;
 
-    public class LongerLine
+    class LongerLine
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine(DistanceCompare());
-        }
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
 
-        public static double LineLengthOne(double xOne1, double xOne2, double yOne1, double yOne2)
-        {
-            double distanceOne = Math.Sqrt(Math.Pow(Math.Abs(xOne1 - xOne2), 2) + Math.Pow(Math.Abs(yOne1 - yOne2), 2));
-            return distanceOne;
-        }
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
 
-        public static double LineLengthTwo(double xTwo1, double xTwo2, double yTwo1, double yTwo2)
-        {
-            double distanceTwo = Math.Sqrt(Math.Pow(Math.Abs(xTwo1 - xTwo2), 2) + Math.Pow(Math.Abs(yTwo1 - yTwo2), 2));
-            return distanceTwo;
-        }
-
-        public static string DistanceCompare()
-        {
-            double xOne1 = double.Parse(Console.ReadLine());
-            double xOne2 = double.Parse(Console.ReadLine());
-            double yOne1 = double.Parse(Console.ReadLine());
-            double yOne2 = double.Parse(Console.ReadLine());
-            double xTwo1 = double.Parse(Console.ReadLine());
-            double xTwo2 = double.Parse(Console.ReadLine());
-            double yTwo1 = double.Parse(Console.ReadLine());
-            double yTwo2 = double.Parse(Console.ReadLine());
-
-            double distanceOne = LineLengthOne(xOne1, xOne2, yOne1, yOne2);
-            double distanceTwo = LineLengthTwo(xTwo1, xTwo2, yTwo1, yTwo2);
-
-            if (distanceOne >= distanceTwo)
+            double lengthOfFirstPair = LineLength(x1, y1, x2, y2);
+            double lengthOfSecondPair = LineLength(x3, y3, x4, y4);
+            string result = string.Empty;
+            if (lengthOfFirstPair >= lengthOfSecondPair)
             {
-                if (Math.Abs(Math.Sqrt(xOne1 * xOne1 + yOne1 * yOne1)) < Math.Abs(Math.Sqrt(xOne2 * xOne2 + yOne2 * yOne2)))
-                {
-                    return string.Format("({0}, {1})({2}, {3}", xOne1, xOne2, yOne1, yOne2);
-                }
-                else if (Math.Abs(Math.Sqrt(xOne1 * xOne1 + yOne1 * yOne1)) > Math.Abs(Math.Sqrt(xOne2 * xOne2 + yOne2 * yOne2)))
-                {
-                    return string.Format("({2}, {3})({0}, {1}", xOne1, xOne2, yOne1, yOne2);
-                }
-                else
-                {
-                    return string.Format("({0}, {1})", xOne1, xOne2);
-                }
+                result = closestPointToCenter(x1, y1, x2, y2);
+                Console.WriteLine(result);
             }
             else
             {
-                if (Math.Abs(Math.Sqrt(xTwo1 * xTwo1 + yTwo1 * yTwo1)) < Math.Abs(Math.Sqrt(xTwo2 * xTwo2 + yTwo2 * yTwo2)))
-                {
-                    return string.Format("({0}, {1})({2}, {3}", xTwo1, xTwo2, yTwo1, yTwo2);
-                }
-                else if (Math.Abs(Math.Sqrt(xTwo1 * xTwo1 + yTwo1 * yTwo1)) > Math.Abs(Math.Sqrt(xTwo2 * xTwo2 + yTwo2 * yTwo2)))
-                {
-                    return string.Format("({2}, {3})({0}, {1})", xTwo1, xTwo2, yTwo1, yTwo2);
-                }
-                else
-                {
-                    return string.Format("({0}, {1})", xTwo1, xTwo2);
-                }
+                result = closestPointToCenter(x3, y3, x4, y4);
+                Console.WriteLine(result);
             }
+        }
+
+        static double LineLength(double x1, double y1, double x2, double y2)
+        {
+            double differenceBetwenX = Math.Abs(x1 - x2);
+            double differenceBetwenY = Math.Abs(y1 - y2);
+            double line = Math.Sqrt(Math.Pow(differenceBetwenX, 2) + Math.Pow(differenceBetwenY, 2));
+            return line;
+        }
+
+        static string closestPointToCenter(double x1, double y1, double x2, double y2)
+        {
+            string coordinates = string.Empty;
+            if (Math.Pow(x1, 2) + Math.Pow(y1, 2) <= Math.Pow(x2, 2) + Math.Pow(y2, 2))
+            {
+                coordinates = $"({x1}, {y1})({x2}, {y2})";
+            }
+            else
+            {
+                coordinates = $"({x2}, {y2})({x1}, {y1})";
+            }
+            return coordinates;
         }
     }
 }
