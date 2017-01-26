cc3e0458d7ad60e2f43b77b6e7e4a47e962ad7a6
Xadera <ragnarokha@gmail.com>
2017-01-26 19:48:38 +0200

    hw

---
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lec/Arrays - Lec/04. TripleSum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.TripleSum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string[] array = n.Split(' ');
+
+
+
+        }
+    }
+}
