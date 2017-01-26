cfa3ccb8e65baeb041531222e5a68d77c681436f
Xadera <ragnarokha@gmail.com>
2017-01-26 14:46:20 +0200

    yey

---
+++ Programming Fundamentals Jan 2017/homework/Methods, Debugging - Lab/Methods, Debugging - Lab/11. GeometryCalculator/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace _11.GeometryCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GeometryCalculator
+    {
+        public static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+
+            switch (figureType)
+            {
+                case "triangle":
+                    double side = double.Parse(Console.ReadLine());
+                    double height = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0:0.00}", TriangleArea(side, height));
+                    break;
+
+                case "square":
+                    double sideSquare = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0:0.00}", SquareArea(sideSquare));
+                    break;
+
+                case "rectangle":
+                    double width = double.Parse(Console.ReadLine());
+                    double heightRectangle = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0:0.00}", RectangleArea(width, heightRectangle));
+                    break;
+
+                case "circle":
+                    double radius = double.Parse(Console.ReadLine());
+                    Console.WriteLine("{0:0.00}", CircleArea(radius));
+                    break;
+            }
+        }
+
+        public static double TriangleArea(double side, double height)
+        {
+            double triangleFace = (side * height) / 2;
+            return triangleFace;
+        }
+
+        public static double SquareArea(double sideSquare)
+        {
+            double squareFace = sideSquare * sideSquare;
+            return squareFace;
+        }
+
+        public static double RectangleArea(double width, double heightRectangle)
+        {
+            double rectangleFace = width * heightRectangle;
+            return rectangleFace;
+        }
+
+        public static double CircleArea(double radius)
+        {
+            double circleArea = Math.PI * radius * radius;
+            return circleArea;
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/homework/Methods, Debugging - Lab/Methods, Debugging - Lab/12. MasterNumbers/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace _12.MasterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MasterNumbers
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (IsPalindrome(i))
+                {
+                    if (ContainsEvenDigit(i))
+                    {
+                        if (SumDivisibleBySeven(i))
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool IsPalindrome(int i)
+        {
+            int n = i;
+            int t = n;
+            int a, s = 0;
+
+            while (n > 0)
+            {
+                a = n % 10;
+                s = (s * 10) + a;
+                n = n / 10;
+            }
+
+            if (t == s)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool ContainsEvenDigit(int i)
+        {
+            bool containsEvenDigit = false;
+
+            for (int n = i; n != 0; n = n / 10)
+            {
+                if ((n % 10) % 2 == 0)
+                {
+                    containsEvenDigit = true;
+                    break;
+                }
+            }
+
+            return containsEvenDigit;
+        }
+
+        public static bool SumDivisibleBySeven(int i)
+        {
+            bool sumDivisibleBySeven = false;
+
+            int sum = 0;
+            for (int n = i; n != 0; n = n / 10)
+            {
+                sum += n % 10;
+            }
+
+            if (sum % 7 == 0)
+            {
+                sumDivisibleBySeven = true;
+            }
+
+            return sumDivisibleBySeven;
+        }
+    }
+}
