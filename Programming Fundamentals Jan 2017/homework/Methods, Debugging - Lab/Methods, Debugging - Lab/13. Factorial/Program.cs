0e80266d67bbe0254ca3cceaaf63e9dfe2e9854f
Xadera <ragnarokha@gmail.com>
2017-01-27 10:10:04 +0200

    fix

--- Programming Fundamentals Jan 2017/homework/Methods, Debugging - Lab/Methods, Debugging - Lab/13. Factorial/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Methods, Debugging - Lab/Methods, Debugging - Lab/13. Factorial/Program.cs
@@ -2,7 +2,8 @@
 {
     using System;
     using System.Numerics;
-    public class Program
+
+    public class FactorialNumber
     {
         public static void Main(string[] args)
         {
@@ -11,7 +12,7 @@
 
             int count = 0;
 
-            for(int i = 5; factorialResult / i >= 1; i *= 5)
+            for (int i = 5; factorialResult / i >= 1; i *= 5)
             {
                 count += n / i;
             }
@@ -21,13 +22,20 @@
 
         public static BigInteger Factorial(int n)
         {
-            BigInteger result = 1;
-            for (int i = 1; i <= n; i++)
+            if (n == 0)
             {
-                result *= i; 
+                return 1;
             }
+            else
+            {
+                BigInteger result = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    result *= i;
+                }
 
-            return result;
+                return result;
+            }
         }
     }
 }
