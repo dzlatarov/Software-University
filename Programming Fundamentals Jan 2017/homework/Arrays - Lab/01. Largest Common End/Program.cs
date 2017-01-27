f53f6b120d5b132b16697af4e81d487fd1e31cc4
Xadera <ragnarokha@gmail.com>
2017-01-27 19:40:13 +0200

    xd

---
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lab/01. Largest Common End/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _01.Largest_Common_End
+{
+    using System;
+
+    public class LargestCommonEnd
+    {
+        public static void Main(string[] args)
+        {
+            string inputOne = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
+
+            string[] arrayOne = inputOne.Split(' ');
+            string[] arrayTwo = inputTwo.Split(' ');
+
+            int shorterArrayLength = arrayOne.Length >= arrayTwo.Length ? arrayTwo.Length : arrayOne.Length;
+            int biggerArrayLength = arrayOne.Length >= arrayTwo.Length ? arrayOne.Length : arrayTwo.Length;
+
+            int leftToRightCount = 0;
+            for (int i = 0; i < shorterArrayLength; i++)
+            {
+                if (arrayOne[i] == arrayTwo[i])
+                {
+                    leftToRightCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            int rightToLeftCount = 0;
+            for (int i = shorterArrayLength - 1; i >= 0; i--)
+            {
+                if (arrayOne.Length >= arrayTwo.Length)
+                {
+                    if (arrayOne[biggerArrayLength - 1] == arrayTwo[shorterArrayLength - 1])
+                    {
+                        rightToLeftCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if (arrayTwo[biggerArrayLength - 1] == arrayOne[shorterArrayLength - 1])
+                    {
+                        rightToLeftCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                biggerArrayLength--;
+                shorterArrayLength--;
+            }
+
+            Console.WriteLine(leftToRightCount >= rightToLeftCount ? leftToRightCount : rightToLeftCount);
+        }
+    }
+}
