2ac46c5dfc382242da3448e1ceb0dcde69585c86
Xadera <ragnarokha@gmail.com>
2017-01-27 14:14:23 +0200

    what have I done

--- Programming Fundamentals Jan 2017/homework/Arrays - Lec/Arrays - Lec/05. RoundingAwayFromZero/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lec/Arrays - Lec/05. RoundingAwayFromZero/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05.RoundingAwayFromZero
+﻿namespace _05.RoundingAwayFromZero
 {
-    class Program
+    using System;
+
+    public class RoundAwayFromZero
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            string inputArray = Console.ReadLine();
+            string[] array = inputArray.Split(' ');
+            double[] doubleArray = new double[array.Length];
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                doubleArray[i] = Convert.ToDouble(array[i]);
+
+                Console.WriteLine($"{doubleArray[i]} => {Math.Round(doubleArray[i], MidpointRounding.AwayFromZero)}");
+            }
         }
     }
 }
---
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lec/Arrays - Lec/07. SumArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _07.SumArrays
+{
+    using System;
+
+    public class SumArrays
+    {
+        public static void Main(string[] args)
+        {
+            string arrayOneInput = Console.ReadLine();
+            string arrayTwoInput = Console.ReadLine();
+            string[] arrayOne = arrayOneInput.Split(' ');
+            string[] arrayTwo = arrayTwoInput.Split(' ');
+
+            int[] arrayOneInt = new int[arrayOne.Length];
+            int[] arrayTwoInt = new int[arrayTwo.Length];
+            int[] arraySum = new int[arrayOneInt.Length >= arrayTwoInt.Length ? arrayOneInt.Length : arrayTwoInt.Length];
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                arrayOneInt[i] = Convert.ToInt32(arrayOne[i]);
+            }
+
+            for (int i = 0; i < arrayTwo.Length; i++)
+            {
+                arrayTwoInt[i] = Convert.ToInt32(arrayTwo[i]);
+            }
+
+            for (int i = 0; i < arraySum.Length; i++)
+            {
+                arraySum[i] = arrayOneInt[i % arrayOneInt.Length] + arrayTwoInt[i % arrayTwoInt.Length];
+                Console.Write($"{arraySum[i]} ");
+            }
+        }
+    }
+}
