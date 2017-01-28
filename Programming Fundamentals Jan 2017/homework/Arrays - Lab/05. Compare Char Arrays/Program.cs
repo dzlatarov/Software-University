40bc14c2dd29ba0c8255184164714b92d0224a5a
Xadera <ragnarokha@gmail.com>
2017-01-28 18:25:50 +0200

    damn those were hard

--- Programming Fundamentals Jan 2017/homework/Arrays - Lab/05. Compare Char Arrays/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lab/05. Compare Char Arrays/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05.Compare_Char_Arrays
+﻿namespace _05.Compare_Char_Arrays
 {
-    class Program
+    using System;
+    using System.Linq;
+
+    public class ComapeCharArrays
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            char[] arrayOne = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] arrayTwo = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            int shorterArraySize = arrayOne.Length >= arrayTwo.Length ? arrayTwo.Length : arrayOne.Length;
+
+            bool isEqual = true;
+
+            for (int i = 0; i < shorterArraySize; i++)
+            {
+                if (arrayOne[i] < arrayTwo[i])
+                {
+                    Console.WriteLine(string.Join("", arrayOne));
+                    Console.WriteLine(string.Join("", arrayTwo));
+                    isEqual = false;
+                    break;
+                }
+                else if (arrayOne[i] > arrayTwo[i])
+                {
+                    Console.WriteLine(string.Join("", arrayTwo));
+                    Console.WriteLine(string.Join("", arrayOne));
+                    isEqual = false;
+                    break;
+                }
+            }
+
+            if (isEqual)
+            {
+                if (arrayOne.Length >= arrayTwo.Length)
+                {
+                    Console.WriteLine(string.Join("", arrayTwo));
+                    Console.WriteLine(string.Join("", arrayOne));
+                }
+                else
+                {
+                    Console.WriteLine(string.Join("", arrayOne));
+                    Console.WriteLine(string.Join("", arrayTwo));
+                }
+            }
         }
     }
 }
