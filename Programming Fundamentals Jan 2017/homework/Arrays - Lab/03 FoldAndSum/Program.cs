f858032299e03cdba37fe0a61f39d05921656307
Xadera <ragnarokha@gmail.com>
2017-01-28 16:53:57 +0200

    hw updg

---
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lab/03 FoldAndSum/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _03_FoldAndSum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> array = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int k = array.Count / 4;
+
+            List<int> rowOneLeft = new List<int>();
+            List<int> rowOneRight = new List<int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                rowOneLeft.Add(array[i]);
+            }
+
+            rowOneLeft.Reverse();
+
+            array.Reverse();
+
+            for (int i = 0; i < k; i++)
+            {
+                rowOneRight.Add(array[i]);
+            }
+
+            array.Reverse();
+
+            List<int> topLine = rowOneLeft.Concat(rowOneRight).ToList();
+
+            int[] result = new int[topLine.Count];
+            for (int i = 0; i < topLine.Count; i++)
+            {
+                result[i] = topLine[i] + array[k + i];
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lab/04. Sieve of Eratosthenes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _04.Sieve_of_Eratosthenes
+{
+    using System;
+
+    public class SieveOfEratosthenes
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            bool[] primeIsTrue = new bool[n + 1];
+
+            for (int i = 2; i <= n; i++)
+            {
+                primeIsTrue[i] = true;
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (primeIsTrue[i])
+                {
+                    for (int p = 2; (p * i) <= n; p++)
+                    {
+                        primeIsTrue[p * i] = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (primeIsTrue[i])
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
