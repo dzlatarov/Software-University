c4c67eab12585e23da2d3d00ebdd685165aa3345
Xadera <ragnarokha@gmail.com>
2017-01-28 13:12:26 +0200

    some more homework

---
+++ Programming Fundamentals Jan 2017/homework/Arrays - Lab/02. Rotate and Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.Rotate_and_Sum
+{
+    using System;
+    using System.Linq;
+
+    public class RotateAndSum
+    {
+        public static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int timesRotated = int.Parse(Console.ReadLine());
+
+            int[] sum = new int[data.Length];
+
+            for (int r = 0; r < timesRotated; r++)
+            {
+                int lastNum = data[data.Length - 1];
+                for (int i = data.Length - 1; i > 0; i--)
+                {
+                    data[i] = data[i - 1];
+                }
+
+                data[0] = lastNum;
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sum[i] += data[i];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
