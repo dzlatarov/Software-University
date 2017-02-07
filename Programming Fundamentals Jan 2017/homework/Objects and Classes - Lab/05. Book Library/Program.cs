0a114a063946f86b48f1f588e5a8e88653aedd40
Xadera <ragnarokha@gmail.com>
2017-02-08 01:19:13 +0200

    xd

--- Programming Fundamentals Jan 2017/homework/Objects and Classes - Lab/05. Book Library/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Objects and Classes - Lab/05. Book Library/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05.Book_Library
+﻿namespace _05.Book_Library
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Library
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Dictionary<string, double> authorRevenue = new Dictionary<string, double>();
+
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ').ToArray();
+                string name = input[1];
+                double price = Convert.ToDouble(input[5]);
+
+                if(!authorRevenue.ContainsKey(name))
+                {
+                    authorRevenue[name] = price;
+                }
+                else
+                {
+                    authorRevenue[name] += price;
+                }
+            }
+
+            foreach(var kvp in authorRevenue.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value:F2}");
+            }
+
+            //why would someone ever use objects for this problem is beyond me
         }
     }
 }
---
+++ Programming Fundamentals Jan 2017/homework/Objects and Classes - Lab/06. Book Library Modification/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.Book_Library_Mod
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Globalization;
+
+    public class LibraryTwo
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, DateTime> titleDate = new Dictionary<string, DateTime>();
+
+            int n = int.Parse(Console.ReadLine());
+            char[] ignore = { ' ' };
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(ignore, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = input[0];
+                DateTime date = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+                titleDate[name] = date;
+            }
+
+            DateTime after = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            foreach (var kvp in titleDate.Where(x => x.Value > after).OrderBy(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value:dd.MM.yyyy}");
+            }
+
+            //why would someone ever use objects for this problem is beyond me
+        }
+    }
+}
