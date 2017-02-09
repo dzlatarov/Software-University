7c442329df7a78f93cee1e6a832abd9a58383005
Xadera <ragnarokha@gmail.com>
2017-02-09 15:04:09 +0200

    hw

---
+++ Programming Fundamentals Jan 2017/homework/Objects and Classes - Lab/09. Teamwork projects/Team.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class Team
+{
+    public string ImeNaEkip { get; set; }
+
+    public string Syzdatel { get; set; }
+
+    public List<string> Uchastnici { get; set; }
+
+    public Team()
+    {
+        this.Uchastnici = new List<string>();
+    }
+}
