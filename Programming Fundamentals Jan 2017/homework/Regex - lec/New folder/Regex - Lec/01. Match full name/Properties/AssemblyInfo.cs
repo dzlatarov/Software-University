50ca439097cbc7557684083dd487d9c71a8c4f28
Xadera <ragnarokha@gmail.com>
2017-02-15 20:02:52 +0200

    YOUR SOUL SHALL SUFFER!

--- Programming Fundamentals Jan 2017/homework/Regex - lec/New folder/Regex - Lec/01. Match full name/Properties/AssemblyInfo.cs
+++ Programming Fundamentals Jan 2017/homework/Regex - lec/New folder/Regex - Lec/01. Match full name/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("memes")]
+[assembly: AssemblyTitle("01. Match full name")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("memes")]
+[assembly: AssemblyProduct("01. Match full name")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b05a41d7-06dc-430c-ad81-340ae27665b9")]
+[assembly: Guid("4e59131c-e0bf-42f3-81d3-ff9c52563321")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Programming Fundamentals Jan 2017/homework/Strings and Text Processing - Lab/05. Magic exchangeable words/Program.cs
+++ Programming Fundamentals Jan 2017/homework/Strings and Text Processing - Lab/05. Magic exchangeable words/Program.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
 
     public class MagicExchangeableWords
     {
@@ -11,19 +12,24 @@
             char[] ignore = new char[] { ' ' };
             string[] input = Console.ReadLine().Split(ignore, StringSplitOptions.RemoveEmptyEntries).ToArray();
 
-            string str1 = input[0];
-            string str2 = input[1];
+            StringBuilder str1 = new StringBuilder(input[0]);
+            StringBuilder str2 = new StringBuilder(input[1]);
             Console.WriteLine(MagicalWords(str1, str2));
         }
 
-        public static bool MagicalWords(string str1, string str2)
+        public static bool MagicalWords(StringBuilder str1, StringBuilder str2)
         {
             List<char> replaced = new List<char>();
             List<char> oldReplaced = new List<char>();
-
+            List<char> smd = new List<char>();
+    
             for (int i = 0; i < str1.Length; i++)
             {
-                if (!replaced.Contains(str2[i])) 
+                if (oldReplaced.Contains(str1[i]))
+                {
+          
+                }
+                else if (!replaced.Contains(str2[i]) )
                 {
                     str1 = str1.Replace(str1[i], str2[i]);
 
@@ -33,7 +39,7 @@
             }
 
             Console.WriteLine(str1);
-            if (str1 == str2)
+            if (string.Join("", str1) == string.Join("", str2))
             {
                 return true;
             }
