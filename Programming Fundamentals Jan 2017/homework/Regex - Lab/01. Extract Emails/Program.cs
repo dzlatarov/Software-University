9a662729895f6eef21186f1893d20479889e3658
Xadera <ragnarokha@gmail.com>
2017-02-16 20:44:59 +0200

    regex is assaids

---
+++ Programming Fundamentals Jan 2017/homework/Regex - Lab/01. Extract Emails/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _01.Extract_Emails
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Text;
+
+    public class EmailExtractor
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder str = new StringBuilder();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    if(input.EndsWith("."))
+                    {
+                        input = input.Remove(input.Length - 1);
+                    }
+                    str.Append(input);
+                }
+            }
+
+            string finalString = str.ToString();
+
+            char[] ignore = new char[] { ' ', ',' };
+            string[] fuckingCancer = finalString.Split(ignore, StringSplitOptions.RemoveEmptyEntries);
+
+            var regex = new Regex(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
+
+            foreach (string cancer in fuckingCancer)
+            {
+                if (regex.IsMatch(cancer))
+                {
+                    Console.WriteLine(cancer);
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/homework/Regex - Lab/02. Extract Sentences By Keyword/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.Extract_Sentences_By_Keyword
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class ExtractSentencesByKeyword
+    {
+        public static void Main(string[] args)
+        {
+            string keyword = Console.ReadLine();
+            string sentences = Console.ReadLine();
+
+            char[] ignore = new char[] { '?', '!', '.', };
+            string[] splitSentences = sentences.Split(ignore).ToArray();
+
+            var nonWords = new Regex(@"[^\w+]");
+            foreach (string str in splitSentences)
+            {
+                string strTemp = nonWords.Replace(str, @" ");
+
+                string[] words = strTemp.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (string word in words)
+                {
+                    if (word == $"{keyword}")
+                    {
+                        Console.WriteLine(str.TrimStart(' '));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
