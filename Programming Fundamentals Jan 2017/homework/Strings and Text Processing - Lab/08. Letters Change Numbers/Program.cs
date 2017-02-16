dabd79df424884efa32c71cc119e0323ea328150
Xadera <ragnarokha@gmail.com>
2017-02-16 17:52:52 +0200

    homework

---
+++ Programming Fundamentals Jan 2017/homework/Strings and Text Processing - Lab/08. Letters Change Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _08.Letters_Change_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class LettersChangeNumbers
+    {
+        public static void Main(string[] args)
+        {
+            char[] ignore = new char[] { ' ', '\t'};
+            string[] input = Console.ReadLine().Split(ignore, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            List<decimal> results = new List<decimal>();
+            foreach (string str in input)
+            {
+                var firstLetterRegex = Regex.Match(str, @"[A-Za-z]");
+                var lastLetterRegex = Regex.Match(str, @"[A-Za-z]$");
+                var numberRegex = Regex.Match(str, @"\d+");
+
+                string firstLetterString = Convert.ToString(firstLetterRegex.ToString());
+                string lastLetterString = Convert.ToString(lastLetterRegex.ToString());
+
+                char firstLetter = firstLetterString[0];
+                char lastLetter = lastLetterString[0];
+                decimal number = Convert.ToDecimal(numberRegex.ToString());
+
+                if (firstLetterString == firstLetterString.ToUpper())
+                {
+                    decimal firstLetterPosition = Convert.ToInt32(firstLetter) - 64;
+                    number = number / firstLetterPosition;
+                }
+                else if (firstLetterString == firstLetterString.ToLower())
+                {
+                    decimal firstLetterPosition = Convert.ToInt32(firstLetter) - 96;
+                    number = number * firstLetterPosition;
+                }
+
+                if (lastLetterString == lastLetterString.ToUpper())
+                {
+                    decimal lastLetterPostion = Convert.ToInt32(lastLetter) - 64;
+                    number = number - lastLetterPostion;
+                }
+                else if (lastLetterString == lastLetterString.ToLower())
+                {
+
+                    decimal lastLetterPosition = Convert.ToInt32(lastLetter) - 96;
+                    number = number + lastLetterPosition;
+                }
+
+                results.Add(number);
+            }
+
+            decimal result = results.Sum();
+
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
