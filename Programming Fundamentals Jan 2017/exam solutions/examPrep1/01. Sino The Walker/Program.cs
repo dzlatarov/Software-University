c51aa6ed6a74fadc8b63a750266a7b284c7ed02d
Xadera <ragnarokha@gmail.com>
2017-02-17 17:05:39 +0200

    yey

---
+++ Programming Fundamentals Jan 2017/exam solutions/examPrep1/01. Sino The Walker/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.Sino_The_Walker
+{
+    using System;
+    using System.Globalization;
+    using System.Numerics;
+
+    public class SinoTheWalker
+    {
+        public static void Main(string[] args)
+        {
+            DateTime simoLeaves = DateTime.ParseExact(Console.ReadLine(), "HH:mm:ss", CultureInfo.InvariantCulture);
+            BigInteger numberOfSteps = BigInteger.Parse(Console.ReadLine());
+            BigInteger timeForEachStep = BigInteger.Parse(Console.ReadLine());
+
+            BigInteger secondsElapsedTotal = numberOfSteps * timeForEachStep;
+            double secondsElapsed = (double) (secondsElapsedTotal % 86400;
+            Console.WriteLine("Time Arrival: {0:HH:mm:ss}", simoLeaves.AddSeconds(secondsElapsed));
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/exam solutions/examPrep1/02. SoftUni Karaoke/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _02.SoftUni_Karaoke
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SoftUniKaroke
+    {
+        public static void Main(string[] args)
+        {
+            string[] officialParticipantNames = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] officialSongNamesUntrimemd = Console.ReadLine().Split(new char[] { ',' }).ToArray();
+
+            List<string> officialSongNames = new List<string>();
+
+            foreach (string str in officialSongNamesUntrimemd)
+            {
+                officialSongNames.Add(str.Trim());
+            }
+
+            Dictionary<string, List<string>> participantAwards = new Dictionary<string, List<string>>();
+            List<string> givenAwards = new List<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "dawn")
+                {
+                    break;
+                }
+                else
+                {
+                    string[] inputSplit = input.Split(',').ToArray();
+
+                    string participantName = inputSplit[0].Trim();
+                    string songName = inputSplit[1].Trim();
+                    string awardName = inputSplit[2].Trim();
+
+                    if (officialParticipantNames.Contains(participantName) && officialSongNames.Contains(songName) && !givenAwards.Contains(awardName))
+                    {
+                        if (participantAwards.ContainsKey(participantName))
+                        {
+                            participantAwards[participantName].Add(awardName);
+                        }
+                        else
+                        {
+
+                            participantAwards[participantName] = new List<string>();
+                            participantAwards[participantName].Add(awardName);
+                        }
+
+                        givenAwards.Add(awardName);
+                    }
+                }
+            }
+
+
+            if (participantAwards.Count() == 0)
+            {
+                Console.WriteLine("No awards");
+            }
+            else
+            {
+                foreach (var kvp in participantAwards.OrderByDescending(x => participantAwards[x.Key].Count()).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{kvp.Key}: {participantAwards[kvp.Key].Count()} awards");
+                    foreach (string str in kvp.Value.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"--{str}");
+                    }
+                }
+            }
+        }
+    }
+}
