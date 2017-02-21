ba952e8529a65041fb9743261bbe0636e921bb05
Xadera <ragnarokha@gmail.com>
2017-02-21 20:37:44 +0200

    И ДОНТ ЕАТ И ДОНТ СЛЕЕП

---
+++ Programming Fundamentals Jan 2017/exam solutions/examPrep2/01. Charity Marathon/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01.Charity_Marathon
+{
+    using System;
+
+    public class CharityMarathon
+    {
+        public static void Main()
+        {
+            long dayCount = long.Parse(Console.ReadLine());
+            long runnerCount = long.Parse(Console.ReadLine());
+            long lapsCount = long.Parse(Console.ReadLine());
+            long lapLength = long.Parse(Console.ReadLine());
+            long trackCapacity = long.Parse(Console.ReadLine());
+            decimal moneyPerKilometer = decimal.Parse(Console.ReadLine());
+
+            if (runnerCount > trackCapacity * dayCount)
+            {
+                runnerCount = trackCapacity * dayCount;
+            }
+
+            decimal totalLengthRan = runnerCount * lapsCount * lapLength;
+            decimal totalLengthRanInKilometers = totalLengthRan / 1000m;
+            decimal result = totalLengthRanInKilometers * moneyPerKilometer;
+
+            Console.WriteLine($"Money raised: {Math.Round(result, 2):f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/exam solutions/examPrep2/02. Ladybugs/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace _02.Ladybugs
+{
+    using System;
+    using System.Linq;
+
+    public class LadyBugs
+    {
+        public static void Main()
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] field = new int[fieldSize];
+            int[] ladyBugs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            foreach (int ladyBug in ladyBugs)
+            {
+                if (ladyBug >= 0 && ladyBug <= ladyBugs.Length - 1)
+                {
+                    field[ladyBug] = 1;
+                }
+            }
+
+            while (true)
+            {
+                string[] token = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                if (token[0] == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    int ladyBug = Convert.ToInt32(token[0]);
+                    string direction = token[1];
+                    int ladyBugStep = Convert.ToInt32(token[2]);
+
+                    if (direction == "left")
+                    {
+                        int ladyBugNewIndex = ladyBug - ladyBugStep;
+                        if (ladyBug >= 0 && ladyBug <= ladyBugs.Length - 1)
+                        {
+                            if (field[ladyBug] != 0)
+                            {
+                                field[ladyBug] = 0;
+
+                                while (true)
+                                {
+                                    if (ladyBugNewIndex < 0 || ladyBugNewIndex > ladyBugs.Length - 1) 
+                                    {
+                                        break;
+                                    }
+                                    else if (ladyBugs[ladyBugNewIndex] == 1)
+                                    {
+                                        if(ladyBugStep > 0)
+                                        {
+                                            ladyBugNewIndex -= ladyBugStep;
+                                        }
+                                        else if (ladyBugStep < 0)
+                                        {
+                                            ladyBugNewIndex += ladyBugStep;
+                                        }
+                                        else
+                                        {
+                                            ladyBugs[ladyBugNewIndex] = 1;
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        field[ladyBugNewIndex] = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        int ladyBugNewIndex = ladyBug + ladyBugStep;
+                        if (ladyBug >= 0 && ladyBug <= field.Length - 1)
+                        {
+                            if (field[ladyBug] != 0)
+                            {
+                                field[ladyBug] = 0;
+
+                                while (true)
+                                {
+                                    if (ladyBugNewIndex < 0 || ladyBugNewIndex > field.Length - 1)
+                                    {
+                                        break;
+                                    }
+                                    else if (field[ladyBugNewIndex] == 1)
+                                    {
+                                        if (ladyBugStep > 0)
+                                        {
+                                            ladyBugNewIndex += ladyBugStep;
+                                        }
+                                        else if (ladyBugStep < 0)
+                                        {
+                                            ladyBugNewIndex -= ladyBugStep;
+                                        }
+                                        else
+                                        {
+                                            field[ladyBugNewIndex] = 1;
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        field[ladyBugNewIndex] = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
