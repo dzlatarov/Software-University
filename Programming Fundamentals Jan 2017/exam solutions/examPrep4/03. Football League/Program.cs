89face14189a6beee34128efa01827cdf4108ac2
Xadera <ragnarokha@gmail.com>
2017-02-24 20:01:27 +0200

    memem magic

---
+++ Programming Fundamentals Jan 2017/exam solutions/examPrep4/03. Football League/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03.Football_League
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FootballLeague
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
