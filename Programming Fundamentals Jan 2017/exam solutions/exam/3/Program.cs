3006fb9ecd7fa9e40e69e2c884ea5f819d0dff57
Xadera <ragnarokha@gmail.com>
2017-02-27 14:54:08 +0200

    меме

---
+++ Programming Fundamentals Jan 2017/exam solutions/exam/3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<long> beehives = Console.ReadLine().Split().Select(long.Parse).ToList();
+            List<long> hornets = Console.ReadLine().Split().Select(long.Parse).ToList();
+
+            List<long> surviningBees = new List<long>();
+
+            long hornetsSum = hornets.Sum();
+            int index = 0;
+            for(int i = 0; i < beehives.Count; i++)
+            {
+                if(index > hornets.Count - 1)
+                {
+                    break;
+                }
+                else if(hornetsSum > beehives[i])
+                {
+
+                }
+                else if(hornetsSum == beehives[i])
+                {
+                    hornetsSum -= hornets[index];
+                    index++;
+                }
+                else if(hornetsSum < beehives[i])
+                {
+                    surviningBees.Add(beehives[i] - hornetsSum);
+                    hornetsSum -= hornets[index];
+                    index++;
+                }
+            }
+
+            if(surviningBees.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", surviningBees));
+            }   
+            else
+            {
+                for(int i = index; i < hornets.Count; i++)
+                {
+                    Console.Write($"{hornets[i]} ");
+                }
+            }
+               
+        }
+    }
+}
---
+++ Programming Fundamentals Jan 2017/exam solutions/exam/4/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace _4
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    public class Armada
+    {
+        public long SoldierCount { get; set; }
+        public int LastActivity { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"(\d+) = (.+) -> (.+):(\d+)");
+            Dictionary<string, Dictionary<string, Armada>> legions = new Dictionary<string, Dictionary<string, Armada>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+
+                int activity = Convert.ToInt32(match.Groups[1].Value);
+                string legionName = match.Groups[2].Value;
+                string soldierType = match.Groups[3].Value;
+                long soldierCount = Convert.ToInt64(match.Groups[4].Value);
+
+                Armada armada = new Armada
+                {
+                    LastActivity = activity,
+                    SoldierCount = soldierCount
+                };
+
+                if (!legions.ContainsKey(legionName))
+                {
+                    legions[legionName] = new Dictionary<string, Armada>();
+                    legions[legionName][soldierType] = armada;
+                }
+                else
+                {
+                    if (!legions[legionName].ContainsKey(soldierType))
+                    {
+                        legions[legionName][soldierType] = armada;
+                    }
+                    else
+                    {
+                        legions[legionName][soldierType].LastActivity = activity;
+                        legions[legionName][soldierType].SoldierCount += soldierCount;
+                    }
+                }
+            }
+            string inputTwo = Console.ReadLine();
+
+            Regex regexOne = new Regex(@"(^\d+)\\(.+)$");
+            Match matchOne = regexOne.Match(inputTwo);
+
+            Regex regexTwo = new Regex(@"(.+)");
+            Match matchTwo = regexTwo.Match(inputTwo);
+
+            if(matchOne.Success)
+            {
+                int tillActivity = Convert.ToInt32(matchOne.Groups[1].Value);
+                string soldierTypeTomatch = matchOne.Groups[2].Value;
+
+                foreach (var kvp in legions)
+                {
+                    foreach (var kvpTwo in kvp.Value.OrderByDescending(x => x.Value.LastActivity))
+                    {
+                        if(kvpTwo.Key == soldierTypeTomatch && kvpTwo.Value.LastActivity < tillActivity)
+                        {
+                            Console.WriteLine($"{kvp.Key} -> {kvpTwo.Value.SoldierCount}");
+                        }
+                    }
+                }
+            }
+            else if(matchTwo.Success)
+            {
+                string soldierTypeTomatch = matchTwo.Groups[1].Value;
+                foreach (var kvp in legions)
+                {
+                    foreach(var kvpTwo in kvp.Value.OrderByDescending(x => x.Value.SoldierCount))
+                    {
+                        if(kvpTwo.Key == soldierTypeTomatch)
+                        {
+                            Console.WriteLine($"{kvpTwo.Value.SoldierCount} : {kvp.Key}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
