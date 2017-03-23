4d1c717e19076f85d0f3fb4cc86d8e3d3701e806
Xadera <ragnarokha@gmail.com>
2017-03-23 18:09:21 +0200

    k

--- Software Technologies Feb 2017/homework/JavaLec/04. Three Integers Sum/04. Three Integers Sum.js
+++ Software Technologies Feb 2017/homework/JavaLec/04. Three Integers Sum/04. Three Integers Sum.js
@@ -1,21 +1,20 @@
-function solve(cunt){
-    let args = cunt[0].split(' ');
+function solve(numbers){
+    let args = numbers[0].split(' ');
+    args = args.sort(function(a,b){return a - b});
 
     let num1 = Number(args[0]);
     let num2 = Number(args[1]);
     let num3 = Number(args[2]);
 
-    if (!(num2 + num3 == num1)) {
-        if (!(num1 + num3 == num2)){
-            if (!(num1 + num2 == num3)) {
-                console.log(args[0]);
-            } else {
-                console.log(`${num1} + ${num2} = ${num3}`)
-            }
-        } else {
-            console.log(`${num1} + ${num3} = ${num2}`);
-        }
-    } else {
+    if (num1 + num2 == num3){
+        console.log(`${num1} + ${num2} = ${num3}`);
+    } else if (num1 + num3 == num2) {
+        console.log(`${num1} + ${num3} = ${num2}`);
+    } else if (num2 + num3 == num1) {
         console.log(`${num2} + ${num3} = ${num1}`);
+    } else {
+        console.log("No");
     }
 }
+
+solve();
