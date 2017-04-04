8dfd010d5b1b335b39570b0cd587696b1e6a1055
Xadera <ragnarokha@gmail.com>
2017-04-04 19:55:41 +0300

    java calc, maybe java is not that bad

---
+++ Software Technologies Feb 2017/homework/Java - Calculator/src/main/java/com/softuni/model/Calculator.java
@@ -0,0 +1,62 @@
+package com.softuni.model;
+
+/**
+ * Created by Lenovo Y700 on 4/4/2017.
+ */
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public Calculator(double leftOperand, double rightOperand, String operator) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+        this.operator = operator;
+    }
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public double calculateResult() {
+        double result;
+        switch (operator) {
+            case "+":
+                result = this.getLeftOperand() + this.getRightOperand();
+                break;
+            case "-":
+                result = this.getLeftOperand() - this.getRightOperand();
+                break;
+            case "*":
+                result = this.getLeftOperand() * this.getRightOperand();
+                break;
+            case "/":
+                result = this.getLeftOperand() / this.getRightOperand();
+                break;
+            default:
+                result = 0;
+        }
+
+        return result;
+    }
+}
