4b0042f9f96983839120f1e7a7cc30ea669feef3
Xadera <ragnarokha@gmail.com>
2017-05-29 14:13:36 +0300

    srubdso unleashed + dragon army - Sets and Dictionaries - Lab/13.  Сръбско Unleashed/Program.cs

--- C# Advanced/Sets and Dictionaries - Lab/Sets and Dictionaries - Lab/13.  Сръбско Unleashed/Program.cs
+++ C# Advanced/Sets and Dictionaries - Lab/Sets and Dictionaries - Lab/13.  Сръбско Unleashed/Program.cs
@@ -2,13 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 public class SrubskoUnleashed
 {
     public static void Main(string[] args)
     {
+        var singersRevenue = new Dictionary<string, Dictionary<string, int>>();
 
+        var matcher = new Regex(@"(.+) @(.+) (\d+) (\d+)");
+
+        while(true)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "End")
+            {
+                break;
+            }
+            else if (matcher.IsMatch(input))
+            {
+                var groups = matcher.Match(input);
+
+                string place = groups.Groups[2].ToString();
+                string singer = groups.Groups[1].ToString();
+                int revenue = int.Parse(groups.Groups[3].ToString()) * int.Parse(groups.Groups[4].ToString());
+                
+                if(!singersRevenue.ContainsKey(place))
+                {
+                    singersRevenue.Add(place, new Dictionary<string, int>());
+                }
+
+                if(!singersRevenue[place].ContainsKey(singer))
+                {
+                    singersRevenue[place].Add(singer, 0);
+                }
+
+                singersRevenue[place][singer] += revenue;
+            }
+        }
+
+        foreach(var place in singersRevenue)
+        {
+            Console.WriteLine(place.Key);
+            foreach (var singer in place.Value.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"#  {singer.Key} -> {singer.Value}");
+            }
+        }
     }
 }
-
--- C# Advanced/Sets and Dictionaries - Lab/Sets and Dictionaries - Lab/14. Dragon Army/Program.cs
+++ C# Advanced/Sets and Dictionaries - Lab/Sets and Dictionaries - Lab/14. Dragon Army/Program.cs
@@ -1,14 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 public class DragonArmy
 {
     public static void Main(string[] args)
     {
+        int numberOfDragons = int.Parse(Console.ReadLine());
 
+        var dragonsByType = new Dictionary<string, SortedDictionary<string, int[]>>();
+
+        var matcher = new Regex(@"(.+) (.+) (\d+|null) (\d+|null) (\d+|null)");
+        
+        for(int i = 0; i < numberOfDragons; i++)
+        {
+            string input = Console.ReadLine();
+
+            if(matcher.IsMatch(input))
+            {
+                var info = matcher.Match(input);
+
+                string type = info.Groups[1].ToString();
+                string name = info.Groups[2].ToString();
+                string damageString = info.Groups[3].ToString();
+                string healthString = info.Groups[4].ToString();
+                string armorString = info.Groups[5].ToString();
+
+                int[] stats = Stats(damageString, healthString, armorString);
+               
+                if(!dragonsByType.ContainsKey(type))
+                {
+                    dragonsByType.Add(type, new SortedDictionary<string, int[]>());
+                }
+
+                if(!dragonsByType[type].ContainsKey(name))
+                {
+                    dragonsByType[type].Add(name, new int[3]);
+                }
+
+                dragonsByType[type][name] = stats;
+            }
+        }
+
+        var averageotalStats = new Dictionary<string, double[]>();
+
+        foreach(var type in dragonsByType)
+        {
+            double totalDamage = 0;
+            double totalHealth = 0;
+            double totalArmor = 0;
+
+            foreach (var dragon in type.Value)
+            {
+                totalDamage += dragon.Value[0];
+                totalHealth += dragon.Value[1];
+                totalArmor += dragon.Value[2];                
+            }
+
+            averageotalStats.Add(type.Key, new double[] { totalDamage / type.Value.Count(), totalHealth / type.Value.Count(), totalArmor / type.Value.Count() });
+        }
+
+        foreach(var type in dragonsByType)
+        {
+            Console.WriteLine($"{type.Key}::({averageotalStats[type.Key][0]:0.00}/{averageotalStats[type.Key][1]:0.00}/{averageotalStats[type.Key][2]:0.00})");
+
+            foreach(var dragon in type.Value)
+            {
+                Console.WriteLine($"-{dragon.Key} -> damage: {dragon.Value[0]}, health: {dragon.Value[1]}, armor: {dragon.Value[2]}");
+            }
+        }
+    }
+
+    public static int[] Stats(string damageString, string healthString, string armorString)
+    {
+        int damage;
+        int health;
+        int armor;
+
+        if (damageString == "null")
+        {
+            damage = 45;
+        }
+        else
+        {
+            damage = int.Parse(damageString);
+        }
+
+        if (healthString == "null")
+        {
+            health = 250;
+        }
+        else
+        {
+            health = int.Parse(healthString);
+        }
+
+        if (armorString == "null")
+        {
+            armor = 10;
+        }
+        else
+        {
+            armor = int.Parse(armorString);
+        }
+
+        return new int[] { damage, health, armor };
     }
 }
-
