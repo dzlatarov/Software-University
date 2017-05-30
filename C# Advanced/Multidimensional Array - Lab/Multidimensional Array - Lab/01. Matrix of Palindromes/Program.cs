4562edbcae8915ec8401b694ed09b1a3b5a61b5d
Xadera <ragnarokha@gmail.com>
2017-05-30 21:25:26 +0300

    problem 1 lab was bored

---
+++ C# Advanced/Multidimensional Array - Lab/Multidimensional Array - Lab/01. Matrix of Palindromes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+
+public class MatrixOfPalindromes
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToArray();
+
+        var x = input[0];
+        var y = input[1];
+        var matrix = new string[x,y];
+
+        var alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        var counterOne = 0;
+        var counterTwo = 0;
+
+        for (var k = 0; k < x; k++)
+        {
+            for (var i = 0; i < y; i++)
+            {
+                var letters = $"{alphabet[counterOne]}{alphabet[counterTwo]}{alphabet[counterOne]}";
+
+                counterTwo++;
+
+                matrix[k, i] = letters;
+            }
+
+            
+            counterOne++;
+            counterTwo = counterOne;
+        }
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int k = 0; k < y; k++)
+            {
+                Console.Write($"{matrix[i,k]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
