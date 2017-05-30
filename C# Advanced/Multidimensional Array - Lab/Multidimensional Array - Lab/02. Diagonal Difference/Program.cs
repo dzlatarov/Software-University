827a577b74012eb88f59d653a4be2e442c6e3cb4
Xadera <ragnarokha@gmail.com>
2017-05-30 22:59:23 +0300

    yoDwag, I heard you like nested loops

---
+++ C# Advanced/Multidimensional Array - Lab/Multidimensional Array - Lab/02. Diagonal Difference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class DiagonalDifference
+{
+    public static void Main(string[] args)
+    {
+        int matrixSize = int.Parse(Console.ReadLine());
+
+        var matrix = new int[matrixSize][];
+
+        for (int i = 0; i < matrixSize; i++)
+        {
+            matrix[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+        }
+
+        int sumLeft = 0;
+        int sumRight = 0;
+
+        for (int space = 0; space < matrixSize; space++)
+        {
+            sumLeft += matrix[space][space];
+            sumRight += matrix[space][matrixSize - space - 1];
+        }
+
+        FindDifference(sumLeft, sumRight);
+    }
+
+    public static void FindDifference(int sumLeft, int sumRight)
+    {
+        if (sumLeft >= sumRight)
+        {
+            Console.WriteLine(sumLeft - sumRight);            
+        }
+        else
+        {
+            Console.WriteLine(sumRight - sumLeft);
+        }
+    }
+}
---
+++ C# Advanced/Multidimensional Array - Lab/Multidimensional Array - Lab/03. 2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public class SquaresInMatrix
+{
+    public static void Main(string[] args)
+    {
+        var matrixDimensions = Console.ReadLine()
+            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToArray();
+
+        int matrixHeight = matrixDimensions[0];
+        int matrixWidth = matrixDimensions[1];
+
+        var matrix = new char[matrixHeight][];
+
+        for (int i = 0; i < matrixHeight; i++)
+        {
+            matrix[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse).ToArray();
+        }
+
+        int counter = 0;
+        for (int x = 0; x < matrixHeight - 1; x++)
+        {
+            for (int y = 0; y < matrixWidth - 1; y++)
+            {
+                if (matrix[x][y] == matrix[x + 1][y] && matrix[x][y] == matrix[x][y + 1] &&matrix[x][y] == matrix[x + 1][y + 1])
+                {
+                    counter++;
+                }
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+}
