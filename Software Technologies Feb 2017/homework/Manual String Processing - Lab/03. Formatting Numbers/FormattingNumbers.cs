b958c1e515e94edd162b8bd4680edd5aeea7a236
Xadera <ragnarokha@gmail.com>
2017-06-07 01:57:26 +0300

    1-8 string processing - lab

---
+++ Software Technologies Feb 2017/homework/Manual String Processing - Lab/03. Formatting Numbers/FormattingNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class FormattingNumbers
+{
+    public static void Main(string[] args)
+    {
+        string[] numbers = Console.ReadLine()
+            .Split(new char[] {' ', '\t'}, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        string stringNumberOne = numbers[0].Trim();
+        string stringNumberTwo = numbers[1].Trim();
+        string stringNumberThree = numbers[2].Trim();
+
+        int numberOne = int.Parse(stringNumberOne);
+        double numberTwo = double.Parse(stringNumberTwo);
+        string numberTwoString = $"{numberTwo:0.00}";
+        double numberThree = double.Parse(stringNumberThree);
+        string doubleThreeString = $"{numberThree:0.000}";
+
+        Console.WriteLine("|{0}|{1}|{2}|{3}|", 
+            numberOne.ToString("X").PadRight(10, ' '),
+            Convert.ToString(numberOne, 2).PadLeft(10, '0').Substring(0, 10),
+            numberTwoString.PadLeft(10, ' '),
+            doubleThreeString.PadRight(10, ' ')
+            );
+    }
+}
---
+++ Software Technologies Feb 2017/homework/Manual String Processing - Lab/06. Count Substring Occurrences/CountSubstringOccurences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+public static class CountSubstringOccurences
+{
+    public static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        string matchSubstring = Console.ReadLine();
+
+        var matcher = new Regex(matchSubstring.ToLower());
+
+        int totalMatches = 0;
+        for (int i = 0; i < text.Length - matchSubstring.Length + 1; i++)
+        {
+            if (matcher.IsMatch(text.Substring(i, matchSubstring.Length).ToLower()))
+            {
+                totalMatches++;
+            }
+        }
+
+        Console.WriteLine(totalMatches);
+    }
+}
