e839ecce8ee6bf449d140036a9b118d3e27c556f
Xadera <ragnarokha@gmail.com>
2017-06-07 12:12:14 +0300

    Manual String Processing - Lab.sln moved files

---
+++ C# Advanced/Manual String Processing - Lab/07. Sum big numbers/SumBigNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+public static class SumBigNumbers
+{
+    public static void Main(string[] args)
+    {
+        BigInteger a = BigInteger.Parse(Console.ReadLine());
+        BigInteger b = BigInteger.Parse(Console.ReadLine());
+
+        BigInteger result = a + b;
+
+        Console.WriteLine(result);
+    }
+}
