aa43246b36dae7712e6ea924f1d3d37f70701d05
Xadera <ragnarokha@gmail.com>
2017-06-07 16:10:10 +0300

    magic exchangeable words

---
+++ C# Advanced/Manual String Processing - Lab/Problem 13. Magic exchangeable words/MagicExchangeableWords.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class MagicExchangeableWords
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine()
+            .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        char[] stringOne = input[0].ToCharArray();
+        char[] stringTwo = input[1].ToCharArray();
+
+        var correspondingLetter = new Dictionary<char, char>();
+
+        if (stringOne.Length == stringTwo.Length)
+        {
+            for (int i = 0; i < stringOne.Length; i++)
+            {
+                char a = stringOne[i];
+                char b = stringTwo[i];
+
+                if (correspondingLetter.ContainsKey(b))
+                {
+                    stringTwo[i] = correspondingLetter[b];
+                }
+                else
+                {
+
+                    correspondingLetter.Add(b, a);
+                }
+            }
+        }
+
+        Console.WriteLine(stringOne);
+        Console.WriteLine(stringTwo);
+        if (stringOne == stringTwo)
+        {
+            Console.WriteLine("true");
+        }
+    }
+}
+
