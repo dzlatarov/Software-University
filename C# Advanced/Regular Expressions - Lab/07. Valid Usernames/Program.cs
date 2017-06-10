134fb3a5c44904205bfe0cff34069cea78f90208
Xadera <ragnarokha@gmail.com>
2017-06-10 14:59:54 +0300

    regex homework remaining problems

--- C# Advanced/Regular Expressions - Lab/07. Valid Usernames/Program.cs
+++ C# Advanced/Regular Expressions - Lab/07. Valid Usernames/Program.cs
@@ -2,13 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 public static class ValidUsernames
 {
     public static void Main(string[] args)
     {
+        string[] userNames = Console.ReadLine()
+            .Split(new char[] {' ', '/', '(', ')', '\\'}, StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
 
+        List<string> filteredUsernames = new List<string>();
+
+        var matcher = new Regex(@"^[a-zA-Z][\w|\d|-]{2,24}$");
+
+        foreach (var username in userNames)
+        {
+            if (matcher.IsMatch(username))
+            {
+                filteredUsernames.Add(username);
+            }
+        }
+
+        int biggestLengthSum = 0;
+        int biggestLengthSumIndex = 0;
+
+        for (int i = 0; i < filteredUsernames.Count - 1; i++)
+        {
+            int currentLength = filteredUsernames[i].Length + filteredUsernames[i + 1].Length;
+
+            if (currentLength > biggestLengthSum)
+            {
+                biggestLengthSum = currentLength;
+                biggestLengthSumIndex = i;
+            }
+        }
+
+        Console.WriteLine(filteredUsernames[biggestLengthSumIndex]);
+        Console.WriteLine(filteredUsernames[biggestLengthSumIndex + 1]);
     }
 }
-
---
+++ C# Advanced/Regular Expressions - Lab/10. Use Your Chains, Buddy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class UseYourChainsBuddy
+{
+    public static void Main()
+    {
+        const int characterOffset = 'n' - 'a';
+        string input = Console.ReadLine();
+
+        Regex symbolsPattern = new Regex(@"[^a-z0-9]+");
+        Regex whitespacePattern = new Regex(@"\s+");
+
+        var matches = Regex.Matches(input, @"<p>(.*?)<\/p>");
+
+        StringBuilder result = new StringBuilder();
+
+        foreach (Match match in matches)
+        {
+            string currentEntry = match.Groups[1].ToString();
+
+            currentEntry = symbolsPattern.Replace(currentEntry, " ");
+            currentEntry = whitespacePattern.Replace(currentEntry, " ");
+
+            foreach (var symbol in currentEntry)
+            {
+                if ('a' <= symbol && symbol <= 'm')
+                {
+                    result.Append((char)(symbol + characterOffset));
+                }
+                else if ('n' <= symbol && symbol <= 'z')
+                {
+                    result.Append((char)(symbol - characterOffset));
+                }
+                else
+                {
+                    result.Append(symbol);
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
