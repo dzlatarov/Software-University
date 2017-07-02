25d2a1426cff48368e63a32b39df05171a52421d
Xadera <ragnarokha@gmail.com>
2017-07-02 20:12:25 +0300

    Some C# OOP basics homework + changed a few names

---
+++ OOP Basics June 2017/Defining Classes - Lec/01. Define Bank Account Class/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BankAccount acc = new BankAccount();
+
+        acc.ID = 1;
+        acc.Balance = 15;
+
+        Console.WriteLine($"Account {acc.ID}, balance {acc.Balance}");
+    }
+}
