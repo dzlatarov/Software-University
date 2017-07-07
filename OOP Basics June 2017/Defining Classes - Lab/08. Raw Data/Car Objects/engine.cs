23712b149271e9d6aedb6f25ef9d2140791b5639
Xadera <ragnarokha@gmail.com>
2017-07-07 15:59:10 +0300

    OOP Basics - Defining Classes - Raw Data

---
+++ OOP Basics June 2017/Defining Classes - Lab/08. Raw Data/Car Objects/engine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Engine
+{
+    private int engineSpeed;
+    private int enginePower;
+
+    public Engine(int EngineSpeed, int EnginePower)
+    {
+        this.engineSpeed = EngineSpeed;
+        this.enginePower = EnginePower;
+    }
+
+    public int EngineSpeed
+    {
+        get { return this.engineSpeed; }
+    }
+
+    public int EnginePower
+    {
+        get { return this.enginePower; }
+    }
+}
+
