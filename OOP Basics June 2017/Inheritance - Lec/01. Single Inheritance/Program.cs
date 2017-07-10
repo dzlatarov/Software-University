08fd7ad631476cefbad30428b88a27ec6a08a1a6
Xadera <ragnarokha@gmail.com>
2017-07-10 16:40:39 +0300

    wtf softuni

---
+++ OOP Basics June 2017/Inheritance - Lec/01. Single Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dog dog = new Dog();
+
+        dog.Eat();
+        dog.Bark();
+    }
+}
+
