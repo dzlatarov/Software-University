c154306aed9faf01630f54cadfc5651716ac0e61
Xadera <ragnarokha@gmail.com>
2017-07-13 17:58:50 +0300

    need for speed beginning

---
+++ OOP Basics June 2017/Exam Prep/Need For Speed/Cars/ShowCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ShowCar : Car
+{
+    private int stars;
+
+    public int Stars
+    {
+        get { return this.stars; }
+    }
+}
+
